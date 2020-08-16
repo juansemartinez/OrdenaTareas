cdb395f1f32712f23997302be4934ad87ba5e5bc
juansemartinez <juansee.martinez@gmail.com>
2020-08-16 12:57:15 -0300

    Updates

---
+++ precache-manifest.e27e7b4814c3512bd2d40c0991a8bff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cbb7e49533552d07760f8a2485ae33",
+    "url": "/OrdenaTareas/index.html"
+  },
+  {
+    "revision": "105f01c3394f6289b1d4",
+    "url": "/OrdenaTareas/static/css/main.e31b4796.chunk.css"
+  },
+  {
+    "revision": "becb66bca24202f8f6e7",
+    "url": "/OrdenaTareas/static/js/2.de03e7c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OrdenaTareas/static/js/2.de03e7c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105f01c3394f6289b1d4",
+    "url": "/OrdenaTareas/static/js/main.d8a69c6a.chunk.js"
+  },
+  {
+    "revision": "204f8b945b99765f43a1",
+    "url": "/OrdenaTareas/static/js/runtime-main.412554d9.js"
+  }
+]);
